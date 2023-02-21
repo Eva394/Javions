42ce81b8e951ec3803e4a673f8d0fcd30913aaa4
nkim7 <nagyungecole@gmail.com>
2023-02-21 14:04:24 +0100

    class maths 2 done

--- Javions/src/ch/epfl/javions/Math2.java
+++ Javions/src/ch/epfl/javions/Math2.java
@@ -1,4 +1,26 @@
 package ch.epfl.javions;
 
-public class Math2 {
+public final class Math2 {
+    private Math2() {}
+
+    public static int clamp(int min, int v, int max) {
+
+        if (min > max) {
+            throw new IllegalArgumentException();
+        }
+
+        else {
+
+            v = Math.min(v,max);
+            v = Math.max(v, min);
+
+
+            return v;
+        }
+    }
+
+    public static double asinh(double x){
+        return Math.log(x + Math.sqrt(Math.pow(x, 2) + 1));
+    }
+
 }
