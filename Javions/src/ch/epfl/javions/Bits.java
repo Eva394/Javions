f4ed857558675e240acc3fa4ef3448aac3b0b957
Eva Mangano <eva.mangano@epfl.ch>
2023-02-26 21:33:30 +0100

    ?

--- Javions/src/ch/epfl/javions/Bits.java
+++ Javions/src/ch/epfl/javions/Bits.java
@@ -12,48 +12,48 @@ import java.util.Objects;
 public class Bits {
 
 
-	private Bits() {
+    private Bits() {
 
-	}
-
-
-	/**
-	 * Extracts a range of bits from the given valuel, from the index start and of given size.
-	 *
-	 * @param value value given form which to extract the range
-	 * @param start index of the start of the range to be extracted
-	 * @param size  size of the range to be extracted
-	 * @return the int value of the extracted range of bits
-	 * @throws IllegalArgumentException if the size is less than 1 or more than 32 (size of an int)
-	 * @author Eva Mangano 345375
-	 */
-	public static int extractUInt(long value, int start, int size) {
-
-		Preconditions.checkArgument( ( size > 0 && size < Integer.SIZE ) );
-		Objects.checkFromIndexSize( start, size, Long.SIZE );
-
-		return (int)( ( value << ( Long.SIZE - ( start + size ) ) >>> ( Long.SIZE - size ) ) );
-	}
+    }
 
 
-	/**
-	 * Returns true if the bit at the index is 1
-	 *
-	 * @param value given value
-	 * @param index index of the bit to look at
-	 * @return a boolean, true if the bit at the index is 1
-	 * @throws IndexOutOfBoundsException if the index is not between 0 (included) and 64 (excluded)
-	 */
-	public static boolean testBit(long value, int index) {
+    /**
+     * Extracts a range of bits from the given valuel, from the index start and of given size.
+     *
+     * @param value value given form which to extract the range
+     * @param start index of the start of the range to be extracted
+     * @param size  size of the range to be extracted
+     * @return the int value of the extracted range of bits
+     * @throws IllegalArgumentException if the size is less than 1 or more than 32 (size of an int)
+     * @author Eva Mangano 345375
+     */
+    public static int extractUInt(long value, int start, int size) {
 
-		Objects.checkIndex( index, Long.SIZE );
+        Preconditions.checkArgument( ( size > 0 && size < Integer.SIZE ) );
+        Objects.checkFromIndexSize( start, size, Long.SIZE );
 
-		long mask = 1L << index;
+        return (int)( ( value << ( Long.SIZE - ( start + size ) ) >>> ( Long.SIZE - size ) ) );
+    }
 
-		if ( ( value & mask ) == mask ) {
-			return true;
-		}
 
-		return false;
-	}
+    /**
+     * Returns true if the bit at the index is 1
+     *
+     * @param value given value
+     * @param index index of the bit to look at
+     * @return a boolean, true if the bit at the index is 1
+     * @throws IndexOutOfBoundsException if the index is not between 0 (included) and 64 (excluded)
+     */
+    public static boolean testBit(long value, int index) {
+
+        Objects.checkIndex( index, Long.SIZE );
+
+        long mask = 1L << index;
+
+        if ( ( value & mask ) == mask ) {
+            return true;
+        }
+
+        return false;
+    }
 }
