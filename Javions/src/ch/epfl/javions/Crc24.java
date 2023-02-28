9974387a6eb8727b08d0ac27e24afe9420a69f39
Eva Mangano <eva.mangano@epfl.ch>
2023-02-28 09:59:26 +0100

    crc24 wtf is this i dont get it

--- Javions/src/ch/epfl/javions/Crc24.java
+++ Javions/src/ch/epfl/javions/Crc24.java
@@ -7,12 +7,50 @@ package ch.epfl.javions;
 
 
 
+/**
+ * Represents a crc calculator oof 24 bits
+ *
+ * @author Eva Mangano 345375
+ */
 public final class Crc24 {
 
-    public static int GENERATOR;
+    /**
+     * Contains the 24 lesser bits of the generator used to calculate the CRC24 of the ADS-B messages of the aircraft
+     */
+    public static int GENERATOR = 0xFFF409;
 
 
-    private Crc24(int generator) {
+    /**
+     * Constructor. Creates an instance of Crc24
+     *
+     * @param generator 24 lesser bits of the generator
+     * @author Eva Mangano 345375
+     */
+    public Crc24(int generator) {
 
+    }
+
+
+    //TODO wtf is this
+    //TODO use the optimized version (3rd)
+    private static Crc24 crc_bitwise(byte[] bytes) {
+/*        int crc = 0;
+
+        byte[] augmentedBytes = new byte[bytes.length + 24];
+
+        for ( int i = 0 ; i < augmentedBytes.length ; i++ ) {
+
+        }*/
+
+        return null;
+    }
+
+
+    /**
+     * @param bytes
+     * @return
+     */
+    public int crc(byte[] bytes) {
+        return 0;
     }
 }
