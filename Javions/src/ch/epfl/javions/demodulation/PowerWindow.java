ab36ab552e4cf4354182cb304dff8200ff923348
Eva Mangano <eva.mangano@epfl.ch>
2023-03-07 19:00:15 +0100

    power window constructor

--- Javions/src/ch/epfl/javions/demodulation/PowerWindow.java
+++ Javions/src/ch/epfl/javions/demodulation/PowerWindow.java
@@ -1,10 +1,24 @@
 package ch.epfl.javions.demodulation;
 
+import ch.epfl.javions.Preconditions;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 public final class PowerWindow {
-    PowerWindow(InputStream stream, int windowSize) throws IOException{
 
+
+    private InputStream stream;
+    private PowerComputer powerComputer;
+    private byte[] tab1;
+    private byte[] tab2;
+    private int windowSize;
+
+
+    public PowerWindow(InputStream stream, int windowSize) throws IOException {
+        Preconditions.checkArgument( ( windowSize > 0 ) && ( windowSize <= ( 1 << 16 ) ) );
+        this.stream = stream;
+        this.powerComputer = new PowerComputer( stream, windowSize );
+        this.windowSize = windowSize;
     }
 }
