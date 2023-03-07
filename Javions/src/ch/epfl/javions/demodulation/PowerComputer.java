7e8e0ff349002f12484374ef1d8d66b9620b09ae
nkim7 <nagyungecole@gmail.com>
2023-03-07 04:16:03 +0100

    powercomputer

--- Javions/src/ch/epfl/javions/demodulation/PowerComputer.java
+++ Javions/src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -7,12 +7,33 @@ import java.io.InputStream;
 
 public final class PowerComputer {
 
-    public PowerComputer(InputStream stream, int batchSize){
+    private int batchSize;
+    private final SamplesDecoder decoder;
+
+
+    public PowerComputer(InputStream stream, int batchSize) {
         Preconditions.checkArgument((batchSize > 0) && (batchSize % 8 == 0));
+
+
+        this.decoder = new SamplesDecoder(stream, batchSize);
+        this.batchSize = batchSize;
     }
 
-    public readBatch(short[] batch){
 
+    public readBatch(short[] batch) throws IOException{
+        Preconditions.checkArgument(batch.length = batchSize);
+
+        short[] samples = new short[batchSize*2];
+        int powerSamples = decoder.readBatch(samples);
+
+
+        for (int i = 0; i < batchSize ; i +=8){
+            int inphase = samples[i + 2] - samples[i + 4] + samples[i + 6] - samples[i];
+            int quadrature = samples[i + 1] - samples[i + 3] + samples[i + 5] - samples[i + 7];
+            batch[i / 8] = (short) (inphase * inphase + quadrature * quadrature);
+        }
+
+        return powerSampels;
 
     }
 }
