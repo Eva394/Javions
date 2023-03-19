c71d807df54d5cc9221fed41bbe567361a91b9be
Eva Mangano <eva.mangano@epfl.ch>
2023-03-19 19:30:11 +0100

    CprDecoder not finished

--- Javions/src/ch/epfl/javions/adsb/CprDecoder.java
+++ Javions/src/ch/epfl/javions/adsb/CprDecoder.java
@@ -7,6 +7,7 @@ package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.GeoPos;
 import ch.epfl.javions.Preconditions;
+import ch.epfl.javions.Units;
 
 /**
  * Decoder of CPR positions
@@ -17,9 +18,40 @@ public class CprDecoder {
     public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent) {
         Preconditions.checkArgument( mostRecent == 0 || mostRecent == 1 );
 
-        int longitude;
-        int latitude;
+        double longitude0;
+        double longitude1;
+        double latitude0;
+        double latitude1;
 
-        return new GeoPos()
+        double nbZonesLat0 = 60;
+        double nbZonesLat1 = 59;
+
+        double widthLat0 = 1 / nbZonesLat0;
+        double widthLat1 = 1 / nbZonesLat1;
+
+        y0 = Math.scalb( y0, -17 );
+        y1 = Math.scalb( y1, -17 );
+
+        double temp = Math.rint( y0 * nbZonesLat1 - y1 * nbZonesLat0 );
+
+        double zoneLat0 = temp;
+        double zoneLat1 = temp;
+
+        if ( temp < 0 ) {
+            zoneLat0 += widthLat0;
+            zoneLat1 += widthLat1;
+        }
+
+        latitude0 = widthLat0 * ( zoneLat0 + y0 );
+        latitude1 = widthLat1 * ( zoneLat1 + y1 );
+
+        if ( latitude > 90 || latitude < -90 ) {
+            return null;
+        }
+
+        double longitudeT32 = Units.convert( longitude, Units.Angle.TURN, Units.Angle.T32 );
+        double latitudeT32 = Units.convert( latitude, Units.Angle.TURN, Units.Angle.T32 );
+
+        return new GeoPos( (int)longitude, (int)latitude );
     }
 }
