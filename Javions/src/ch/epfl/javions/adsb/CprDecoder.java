4a55606c5868c21ec80aa167104d2b0b30d8919a
Eva Mangano <eva.mangano@epfl.ch>
2023-03-21 08:46:09 +0100

    ?

--- Javions/src/ch/epfl/javions/adsb/CprDecoder.java
+++ Javions/src/ch/epfl/javions/adsb/CprDecoder.java
@@ -4,10 +4,13 @@ package ch.epfl.javions.adsb;
  *  Date :          19/03/2023
  */
 
+import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.Preconditions;
+
 /**
  * Decoder of CPR positions
  */
-/*public class CprDecoder {
+public class CprDecoder {
 
 
     public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent) {
@@ -18,11 +21,11 @@ package ch.epfl.javions.adsb;
         double latitude0;
         double latitude1;
 
-        double nbZonesLat0 = 60;
-        double nbZonesLat1 = 59;
+        double nbZonesLat0 = 60.;
+        double nbZonesLat1 = 59.;
 
-        double widthLat0 = 1 / nbZonesLat0;
-        double widthLat1 = 1 / nbZonesLat1;
+        double widthLat0 = 1. / nbZonesLat0;
+        double widthLat1 = 1. / nbZonesLat1;
 
         y0 = Math.scalb( y0, -17 );
         y1 = Math.scalb( y1, -17 );
@@ -40,14 +43,14 @@ package ch.epfl.javions.adsb;
         latitude0 = widthLat0 * ( zoneLat0 + y0 );
         latitude1 = widthLat1 * ( zoneLat1 + y1 );
 
-        if ( latitude > 90 || latitude < -90 ) {
-            return null;
-        }
-
-        double longitudeT32 = Units.convert( longitude, Units.Angle.TURN, Units.Angle.T32 );
-        double latitudeT32 = Units.convert( latitude, Units.Angle.TURN, Units.Angle.T32 );
-
-        return new GeoPos( (int)longitude, (int)latitude );
+        //        if ( latitude0 > 90 || latitude0 < -90 ) {
+        //            return null;
+        //        }
+        //
+        //        double longitudeT32 = Units.convert( longitude, Units.Angle.TURN, Units.Angle.T32 );
+        //        double latitudeT32 = Units.convert( latitude, Units.Angle.TURN, Units.Angle.T32 );
+        //
+        //        return new GeoPos( (int)longitude, (int)latitude );
+        return null;
     }
 }
-*/
---
+++ Javions/test/ch/epfl/javions/adsb/CprDecoderTest.java
@@ -0,0 +1,6 @@
+package ch.epfl.javions.adsb;
+
+class CprDecoderTest {
+
+
+}
