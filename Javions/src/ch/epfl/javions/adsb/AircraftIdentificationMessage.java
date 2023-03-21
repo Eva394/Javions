5d76b6152d18b416cc1517d6a06fba7aaeffd22a
nkim7 <nagyungecole@gmail.com>
2023-03-21 13:18:52 +0100

    Identification

--- Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
@@ -1,13 +1,12 @@
 package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.Preconditions;
-import ch.epfl.javions.aircraft.AircraftTypeDesignator;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
-public record AircraftIdentificationMessage(long timeStampNs,IcaoAddress icaoAddress, int category, CallSign callSign) implements Message{
+public record AircraftIdentificationMessage(long timeStampNs,IcaoAddress icaoAddress, int category, CallSign callSign) implements Message {
 
-    public AircraftIdentificationMessage(){
-        Preconditions.checkArgument(timeStampNs >=0);
+    public AircraftIdentificationMessage() {
+        Preconditions.checkArgument(timeStampNs >= 0);
 
         if (icaoAddress == null || callSign == null) {
             throw new NullPointerException();
@@ -15,17 +14,71 @@ public record AircraftIdentificationMessage(long timeStampNs,IcaoAddress icaoAdd
 
     }
 
-    public AircraftIdentificationMessage of(RawMessage rawMessage){
+    public AircraftIdentificationMessage of(RawMessage rawMessage) {
+
+        /*
+        if (rawMessage.typeCode() != 1 && rawMessage.typeCode() != 2 && rawMessage.typeCode() != 3 && rawMessage.typeCode() != 4) {
+            return null;
+        }
+
+         */
+
+        String strCA = rawMessage.toString().substring(48, 51);
+        int intCA = Integer.parseInt(strCA, 2);
+        String CA = Integer.toHexString(intCA);
+        int typeCode = rawMessage.typeCode();
+        int temp = 14 - typeCode;
+
+        String Category = Integer.toHexString(temp) + CA;
 
 
-        for (int i = 0; i < 48; i +=6){
 
-            int nb =
+
+        String CallSign = rawMessage.toString().substring(0, 47);
+
+        for (int i = 0; i < 8; i++) {
+
+            String a = rawMessage.toString().substring(42 - (6 * i), 42 - (6 * i) + 5);
+            int character = (int) Integer.parseInt(a, 2);
+
+            if (character == 0 || (character >= 27 && character <= 47)) {
+                return null;
+            }
 
         }
 
-
+        return new AircraftIdentificationMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), Category,callSign);
     }
-
-
 }
+
+
+
+
+        /*
+
+
+        char[] callSign = new char[8];
+
+        for (int i=0; i<8; i++){
+            int callSign = (int) rawMessage.get(42 - (6 * i), 6);
+        }
+
+        char[] C1 = new char[6];
+        rawMessage.toString().getChars(42,47,C1,0);
+
+        String CA = rawMessage.toString().substring(48, 51);
+
+        String C2 = rawMessage.toString().substring(36, 41);
+        String C3 = rawMessage.toString().substring(30, 45);
+        String C4 = rawMessage.toString().substring(24, 29);
+        String C5 = rawMessage.toString().substring(18, 23);
+        String C6 = rawMessage.toString().substring(12, 17);
+        String C7 = rawMessage.toString().substring(6, 11);
+        String C8 = rawMessage.toString().substring(0, 7);
+
+
+
+         */
+
+
+
