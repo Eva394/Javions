10f71f164909044e2a2f5beccaa20933e182f57e
Eva Mangano <eva.mangano@epfl.ch>
2023-03-21 10:01:07 +0100

    CprDecoder

--- Javions/test/ch/epfl/javions/adsb/CprDecoderTest.java
+++ Javions/test/ch/epfl/javions/adsb/CprDecoderTest.java
@@ -1,6 +1,11 @@
 package ch.epfl.javions.adsb;
 
+import org.junit.jupiter.api.Test;
+
 class CprDecoderTest {
 
-
+    @Test
+    void justRun() {
+        CprDecoder.decodePosition( 111600., 94445., 108865., 77558., 0 );
+    }
 }
