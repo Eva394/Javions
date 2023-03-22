fc8b0d109f67a6bd2ed70a450f5eed19214087f5
Eva Mangano <eva.mangano@epfl.ch>
2023-03-22 16:38:54 +0100

    nearly finished mb ?

--- Javions/src/ch/epfl/javions/adsb/AirbornePositionMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AirbornePositionMessage.java
@@ -12,6 +12,10 @@ import ch.epfl.javions.aircraft.IcaoAddress;
 public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress, double altitude, int parity, double x,
                                       double y) implements Message {
 
+
+    private static final int ME_SIZE = 12;
+
+
     public AirbornePositionMessage {
         if ( icaoAddress == null ) {
             throw new NullPointerException();
@@ -34,19 +38,42 @@ public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress,
             alt = -1000 + a * 25;
         }
         else {
-            int[] tangled = new int[]{Bits.extractUInt( attributeALT, 2, 1 ), Bits.extractUInt( attributeALT, 0, 1 ),
-                                      Bits.extractUInt( attributeALT, 10, 1 ), Bits.extractUInt( attributeALT, 8, 1 ),
-                                      Bits.extractUInt( attributeALT, 6, 1 ), Bits.extractUInt( attributeALT, 5, 1 ),
-                                      Bits.extractUInt( attributeALT, 3, 1 ), Bits.extractUInt( attributeALT, 1, 1 ),
-                                      Bits.extractUInt( attributeALT, 11, 1 ), Bits.extractUInt( attributeALT, 9, 1 ),
-                                      Bits.extractUInt( attributeALT, 7, 1 )};
+            int untangled = untangle( attributeALT );
+            int multipleOf500 = Bits.extractUInt( untangled, 3, 9 );
+            int multipleOf100 = Bits.extractUInt( untangled, 0, 3 );
 
-            int untangled = 0;
-            for ( int i = 12 ; i > 0 ; i-- ) {
-                untangled = ( untangled << i - 1 ) | ( tangled[i] );
-            }
+            int msBits = decodeGrey( multipleOf500, 9 );
+            int lsBits = decodeGrey( multipleOf100, 3 );
         }
 
         return alt;
+    }
+
+
+    private static int decodeGrey(int value, int n) {
+
+        int binaryValue = value;
+
+        for ( int i = 1 ; i < n ; i++ ) {
+            binaryValue = binaryValue ^ ( value >> i );
+        }
+
+        return binaryValue;
+    }
+
+
+    private static int untangle(int attributeALT) {
+        int[] tangled = new int[]{Bits.extractUInt( attributeALT, 2, 1 ), Bits.extractUInt( attributeALT, 0, 1 ),
+                                  Bits.extractUInt( attributeALT, 10, 1 ), Bits.extractUInt( attributeALT, 8, 1 ),
+                                  Bits.extractUInt( attributeALT, 6, 1 ), Bits.extractUInt( attributeALT, 5, 1 ),
+                                  Bits.extractUInt( attributeALT, 3, 1 ), Bits.extractUInt( attributeALT, 1, 1 ),
+                                  Bits.extractUInt( attributeALT, 11, 1 ), Bits.extractUInt( attributeALT, 9, 1 ),
+                                  Bits.extractUInt( attributeALT, 7, 1 )};
+
+        int untangled = 0;
+        for ( int i = ME_SIZE - 2 ; i > 0 ; i -= 1 ) {
+            untangled = ( untangled ) | ( tangled[i] << ( i - 1 ) );
+        }
+        return untangled;
     }
 }
