35ace04952ff805a04be0683918c8bf8615a5447
nkim7 <nagyungecole@gmail.com>
2023-03-26 04:41:45 +0200

    Identification Message Fixed

--- Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
@@ -24,33 +24,11 @@ public record AircraftIdentificationMessage(long timeStampNs, IcaoAddress icaoAd
 
         int typeCode = rawMessage.typeCode();
 
-        /*
-        if (typeCode != 1 && typeCode != 2 && typeCode != 3 && typeCode != 4) {
-            return null;
-        }
-
-         */
 
         int CA = Bits.extractUInt( rawMessage.payload(), 48, 3 );
         int temp = 0xE - typeCode;
         int category = ( temp << 4 ) | CA;
 
-        /*
-        int categoryDecimal = CA << 1 | temp;
-        int categroy = Integer.
-         */
-        /*
-
-        String strCategory = Integer.toHexString(temp).toUpperCase() + Integer.toHexString(CA).toUpperCase();
-        int category = Integer.parseInt(strCategory, 16);
-
-         */
-
-        /*
-
-        int callSign = Bits.extractUInt(rawMessage.payload(),0,48);
-
-         */
 
         StringBuilder cS = new StringBuilder( 6 );
 
@@ -62,13 +40,6 @@ public record AircraftIdentificationMessage(long timeStampNs, IcaoAddress icaoAd
                 cS.append( c );
             }
 
-            /*
-
-            if (character == 0 || (character >= 27 && character <= 47)) {
-                return null;
-            }
-
-             */
         }
 
         String callSignStr = cS.reverse()
@@ -84,60 +55,4 @@ public record AircraftIdentificationMessage(long timeStampNs, IcaoAddress icaoAd
                                                   callSign );
     }
 }
-
-
-        /*
-
-        String strCA = rawMessage.toString().substring(48, 50);
-        int intCA = Integer.parseInt(strCA,2);
-        int typeCode = rawMessage.typeCode();
-        int temp = 14 - typeCode;
-
-        String strCategory = Integer.toHexString(temp).toUpperCase() + Integer.toHexString(intCA).toUpperCase();
-
-        int category = Integer.parseInt(strCategory, 16);
-
-
-        String callSignString = rawMessage.toString().substring(0, 47);
-        CallSign callSign = new CallSign(callSignString);
-
-        for (int i = 0; i < 8; i++) {
-
-            String a = rawMessage.toString().substring(48 - (6 * i), 48 - (6 * i)+5 );
-            int character = (int) Integer.parseInt(a, 2);
-
-            if (character == 0 || (character >= 27 && character <= 47)) {
-                return null;
-            }
-
-        }
-
-         */
-
-
-        /*
-        char[] callSign = new char[8];
-
-        for (int i=0; i<8; i++){
-            int callSign = (int) rawMessage.get(42 - (6 * i), 6);
-        }
-
-        char[] C1 = new char[6];
-        rawMessage.toString().getChars(42,47,C1,0);
-
-        String CA = rawMessage.toString().substring(0, 2);
-        String C1 = rawMessage.toString().substring(3, 8);
-        String C2 = rawMessage.toString().substring(9, 14);
-        String C3 = rawMessage.toString().substring(15, 20);
-        String C4 = rawMessage.toString().substring(21, 26);
-        String C5 = rawMessage.toString().substring(27, 32);
-        String C6 = rawMessage.toString().substring(33, 38);
-        String C7 = rawMessage.toString().substring(39, 44);
-        String C8 = rawMessage.toString().substring(45, 50);
-
-
-
-         */
-
-
 
