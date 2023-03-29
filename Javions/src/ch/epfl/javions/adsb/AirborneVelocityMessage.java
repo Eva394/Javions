59d5a236fc9544a1a6e22e532d1d8bb9c7625154
Eva Mangano <eva.mangano@epfl.ch>
2023-03-29 13:49:20 +0200

    doesnt work

--- Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -5,68 +5,89 @@ import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.Units;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
-import static ch.epfl.javions.Units.Angle.DEGREE;
-import static ch.epfl.javions.Units.Speed.KNOT;
+import java.util.Objects;
 
-public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
-                                      double speed, double trackOrHeading) implements Message {
+public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress, double speed,
+                                      double trackOrHeading) implements Message {
 
     public AirborneVelocityMessage {
-        if (icaoAddress == null) {
-            throw new NullPointerException();
-        }
-
-        Preconditions.checkArgument((timeStampNs >= 0) && (speed >= 0) && (trackOrHeading >= 0));
+        Objects.requireNonNull( icaoAddress );
+        Preconditions.checkArgument( ( timeStampNs >= 0 ) && ( speed >= 0 ) && ( trackOrHeading >= 0 ) );
     }
+
 
     public static AirborneVelocityMessage of(RawMessage rawMessage) {
 
-        int SB = Bits.extractUInt(rawMessage.payload(), 48, 3);
-        int Dew = Bits.extractUInt(rawMessage.payload(), 21, 1);
-        int Vew = Bits.extractUInt(rawMessage.payload(), 11, 10);
-        int Dns = Bits.extractUInt(rawMessage.payload(), 10, 1);
-        int Vns = Bits.extractUInt(rawMessage.payload(), 0, 10);
+        int subType = Bits.extractUInt( rawMessage.payload(), 48, 3 );
 
-        int SH = Bits.extractUInt(rawMessage.payload(), 21, 1);
-        int HDG = Bits.extractUInt(rawMessage.payload(), 11, 10);
-        int T = Bits.extractUInt(rawMessage.payload(), 10, 1);
-        int AS = Bits.extractUInt(rawMessage.payload(), 0, 10);
+        if ( subType == 1 || subType == 2 ) {
+            int directionEW = Bits.extractUInt( rawMessage.payload(), 21, 1 );
+            int velocityEW = Bits.extractUInt( rawMessage.payload(), 11, 10 );
+            int directionNS = Bits.extractUInt( rawMessage.payload(), 10, 1 );
+            int velocityNS = Bits.extractUInt( rawMessage.payload(), 0, 10 );
 
-
-        if (SB == 1 || SB == 2) {
-            if (Vns == 0 || Vew == 0) {
+            if ( velocityNS == 0 || velocityEW == 0 ) {
                 return null;
             }
 
-            double angle = Units.convertTo(Math.atan2(1 - Dns, 1 - Dew), DEGREE);
-            double speed;
+            double angle = Math.atan2( directionNS, directionEW );
+            double speed = Math.hypot( velocityNS - 1, velocityEW - 1 );
 
-            if (SB == 1) {
-                speed = Units.convertTo(Math.hypot(Vns + 1, Vew + 1), KNOT);
-            } else {
-                speed = Units.convertTo(4, KNOT);
+            if ( subType == 1 ) {
+                speed = Units.convertFrom( speed, Units.Speed.KNOT );
+            }
+            else {
+                speed = Units.convertFrom( speed * 4., Units.Speed.KNOT );
             }
 
-            return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speed, angle);
+            //TODO angle
+
+            //            double angle = Units.convertTo( Math.atan2( 1 - directionNS, 1 - directionEW ), DEGREE );
+            //            double speed;
+            //
+            //            if ( subType == 1 ) {
+            //                speed = Units.convertTo( Math.hypot( velocityNS - 1, velocityEW - 1 ), KNOT );
+            //            }
+            //            else {
+            //                speed = Units.convertTo( Math.hypot( velocityNS - 1, velocityEW - 1 ) / 4., KNOT );
+            //            }
+            //
+            return new AirborneVelocityMessage( rawMessage.timeStampNs(), rawMessage.icaoAddress(), speed, angle );
         }
 
-        if (SB == 3 || SB == 4) {
-            if (AS == 0){
+        if ( subType == 3 || subType == 4 ) {
+            int headingAvailability = Bits.extractUInt( rawMessage.payload(), 21, 1 );
+            int heading = Bits.extractUInt( rawMessage.payload(), 11, 10 );
+            int airVelocity = Bits.extractUInt( rawMessage.payload(), 0, 10 );
+
+            if ( airVelocity == 0 || heading != 1 ) {
                 return null;
             }
 
-            double angle = (HDG * 1.0) / (1 << 10);
+            double angle = (double)heading / ( 1 << 10 );
             double speed;
 
-            if (SH == 1 && SB == 3) {
-                speed = AS;
-            } else if (SH == 1) {
-                speed = Units.convertTo(4, KNOT);
-            } else {
-                return null;
+            if ( subType == 3 ) {
+                speed = Units.convertFrom( airVelocity - 1, Units.Speed.KNOT );
+            }
+            else {
+                speed = Units.convertFrom( ( airVelocity - 1 ) * 4., Units.Speed.KNOT );
             }
 
-            return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speed, angle);
+            //            double angle = (double)( heading ) / ( 1 << 10 );
+            //            double speed;
+            //
+            //            if ( headingAvailability == 1 && subType == 3 ) {
+            //                speed = airVelocity;
+            //            }
+            //            else if ( headingAvailability == 1 ) {
+            //                speed = Units.convertTo( 4, KNOT );
+            //            }
+            //            else {
+            //                return null;
+            //            }
+            //
+            return new AirborneVelocityMessage( rawMessage.timeStampNs(), rawMessage.icaoAddress(), speed, angle );
         }
 
         return null;
