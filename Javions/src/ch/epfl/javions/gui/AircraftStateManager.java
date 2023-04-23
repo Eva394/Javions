0229dbe282c3a1c9f48fff258a7ccc8e629bbc6b
Eva Mangano <eva.mangano@epfl.ch>
2023-04-23 12:13:21 +0200

    AircraftStateManager javadoc

--- Javions/src/ch/epfl/javions/gui/AircraftStateManager.java
+++ Javions/src/ch/epfl/javions/gui/AircraftStateManager.java
@@ -16,36 +16,53 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Manages the states of the aircrafts. Keeps the states of all the aircrafts up tp date with the messages received
+ */
 public final class AircraftStateManager {
 
     private final static double ONE_MINUTE_IN_NANOSECONDS = 6 * ( 1e10 );
     private final Map<IcaoAddress, AircraftStateAccumulator<ObservableAircraftState>> receivedStates;
     private final ObservableSet<ObservableAircraftState> knownStates;
-    //private final ObservableSet<ObservableAircraftState> unmodifiableKnownStates;
+    private final ObservableSet<ObservableAircraftState> unmodifiableKnownStates;
     private final AircraftDatabase aircraftDatabase;
     private long lastUpdatedTimeStampNs;
 
 
+    /**
+     * Constructor. Builds an instance of <code>AircraftStateManager</code>
+     * @param aircraftDatabase database for mictronics data of the aircrafts
+     */
     public AircraftStateManager(AircraftDatabase aircraftDatabase) {
         this.aircraftDatabase = aircraftDatabase;
         this.receivedStates = new HashMap<>();
         this.knownStates = FXCollections.observableSet();
         this.lastUpdatedTimeStampNs = 0L;
-
-        //this.unmodifiableKnownStates = ;
+        this.unmodifiableKnownStates = FXCollections.unmodifiableObservableSet( knownStates );
     }
 
 
+    /**
+     * Getter for an unmodifiable wrapper list on top of the list <code>knownStates</code>
+     * (<code>unmodifiableKnownStates</code>)
+     * @return an unmodifiable wrapper list on top of observable list of known states <code>knownStates</code>
+     */
     public ObservableSet<ObservableAircraftState> states() {
-        return FXCollections.unmodifiableObservableSet( knownStates );
-        //TODO redo what julain made me do (create attribute for this and initialize in constructor
-        // return unmodifiableKnownStates;
+        return unmodifiableKnownStates;
     }
 
 
+    /**
+     * Updates the state of the aircraft from which we received the <code>message</code> to the new state contained in
+     * the aforementioned message
+     * @param message message received from an aircraft
+     * @throws IOException if there is an input/output error
+     */
     public void updateWithMessage(Message message) throws IOException {
 
+        lastUpdatedTimeStampNs = message.timeStampNs();
         IcaoAddress icaoAddress = message.icaoAddress();
+
         if ( receivedStates.containsKey( icaoAddress ) ) {
             AircraftStateAccumulator<ObservableAircraftState> stateAccumulator = receivedStates.get( icaoAddress );
             stateAccumulator.update( message );
@@ -59,14 +76,16 @@ public final class AircraftStateManager {
                                                                                  aircraftDatabase.get( icaoAddress ) );
             AircraftStateAccumulator<ObservableAircraftState> newStateAccumulator = new AircraftStateAccumulator<>(
                     aircraftState );
-            //if smth doesnt work delete the next line
             newStateAccumulator.update( message );
             receivedStates.put( icaoAddress, newStateAccumulator );
         }
-        lastUpdatedTimeStampNs = message.timeStampNs();
     }
 
 
+    /**
+     * Deletes the states of the aircrafts from which no message has been received for one minute or more since the last
+     * message. Deletes it form <code>knownStates</code>
+     */
     public void purge() {
         knownStates.removeIf(
                 state -> lastUpdatedTimeStampNs - ONE_MINUTE_IN_NANOSECONDS > state.getLastMessageTimeStampNs() );
