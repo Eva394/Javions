9013e9cf5234ee5e4638b5351e7937cca9e48aaf
nkim7 <nagyungecole@gmail.com>
2023-05-01 02:44:05 +0200

    colrRamp done...?

---
+++ Javions/src/ch/epfl/javions/gui/AircraftIcon.java
@@ -0,0 +1,23 @@
+package ch.epfl.javions.gui;
+
+import ch.epfl.javions.aircraft.AircraftDescription;
+import ch.epfl.javions.aircraft.AircraftTypeDesignator;
+import ch.epfl.javions.aircraft.WakeTurbulenceCategory;
+import javafx.scene.shape.SVGPath;
+
+public enum AircraftIcon {
+
+    public boolean canRotate(){
+
+    }
+
+    public String svgPath(){
+    }
+
+    public static AircraftIcon iconFor(AircraftTypeDesignator typeDesignator,
+                                       AircraftDescription typeDescription,
+                                       int category,
+                                       WakeTurbulenceCategory wakeTurbulenceCategory){
+
+    }
+}
