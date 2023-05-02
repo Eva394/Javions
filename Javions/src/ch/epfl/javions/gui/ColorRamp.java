7affe1c8590d8486e0e01765006ad38220ef1215
Eva Mangano <eva.mangano@epfl.ch>
2023-05-02 17:21:10 +0200

    todo added in ColorRamp

--- Javions/src/ch/epfl/javions/gui/ColorRamp.java
+++ Javions/src/ch/epfl/javions/gui/ColorRamp.java
@@ -5,25 +5,60 @@ import javafx.scene.paint.Color;
 
 public final class ColorRamp {
 
+    public static final ColorRamp PLASMA = new ColorRamp( Color.valueOf( "0x0d0887ff" ),
+                                                          Color.valueOf( "0x220690ff" ),
+                                                          Color.valueOf( "0x320597ff" ),
+                                                          Color.valueOf( "0x40049dff" ),
+                                                          Color.valueOf( "0x4e02a2ff" ),
+                                                          Color.valueOf( "0x5b01a5ff" ),
+                                                          Color.valueOf( "0x6800a8ff" ),
+                                                          Color.valueOf( "0x7501a8ff" ),
+                                                          Color.valueOf( "0x8104a7ff" ),
+                                                          Color.valueOf( "0x8d0ba5ff" ),
+                                                          Color.valueOf( "0x9814a0ff" ),
+                                                          Color.valueOf( "0xa31d9aff" ),
+                                                          Color.valueOf( "0xad2693ff" ),
+                                                          Color.valueOf( "0xb6308bff" ),
+                                                          Color.valueOf( "0xbf3984ff" ),
+                                                          Color.valueOf( "0xc7427cff" ),
+                                                          Color.valueOf( "0xcf4c74ff" ),
+                                                          Color.valueOf( "0xd6556dff" ),
+                                                          Color.valueOf( "0xdd5e66ff" ),
+                                                          Color.valueOf( "0xe3685fff" ),
+                                                          Color.valueOf( "0xe97258ff" ),
+                                                          Color.valueOf( "0xee7c51ff" ),
+                                                          Color.valueOf( "0xf3874aff" ),
+                                                          Color.valueOf( "0xf79243ff" ),
+                                                          Color.valueOf( "0xfa9d3bff" ),
+                                                          Color.valueOf( "0xfca935ff" ),
+                                                          Color.valueOf( "0xfdb52eff" ),
+                                                          Color.valueOf( "0xfdc229ff" ),
+                                                          Color.valueOf( "0xfccf25ff" ),
+                                                          Color.valueOf( "0xf9dd24ff" ),
+                                                          Color.valueOf( "0xf5eb27ff" ),
+                                                          Color.valueOf( "0xf0f921ff" ) );
     private final Color[] colors;
 
 
-    public ColorRamp(Color...colors){
+    public ColorRamp(Color... colors) {
 
-        Preconditions.checkArgument(colors.length<2);
+        Preconditions.checkArgument( colors.length < 2 );
         this.colors = colors;
-
     }
 
+
     public Color at(double value) {
-        if (value <= 0) {
+        //TODO clamp
+        if ( value <= 0 ) {
             return colors[0];
-        } else if (value >= 1) {
+        }
+        else if ( value >= 1 ) {
             return colors[colors.length - 1];
-        } else {
-            int i = (int) (value * (colors.length - 1));
-            double location = (value - i * 1.0 / (colors.length - 1)) * (colors.length - 1);
-            return colors[i].interpolate(colors[i+1], location);
+        }
+        else {
+            int i = (int)( value * ( colors.length - 1 ) );
+            double location = ( value - i * 1.0 / ( colors.length - 1 ) ) * ( colors.length - 1 );
+            return colors[i].interpolate( colors[i + 1], location );
         }
     }/*
     public Color at(double value) {
@@ -40,23 +75,4 @@ public final class ColorRamp {
         return c1.interpolate(c2, fraction);
     }
     */
-
-    public static final ColorRamp PLASMA = new ColorRamp(
-            Color.valueOf("0x0d0887ff"), Color.valueOf("0x220690ff"),
-            Color.valueOf("0x320597ff"), Color.valueOf("0x40049dff"),
-            Color.valueOf("0x4e02a2ff"), Color.valueOf("0x5b01a5ff"),
-            Color.valueOf("0x6800a8ff"), Color.valueOf("0x7501a8ff"),
-            Color.valueOf("0x8104a7ff"), Color.valueOf("0x8d0ba5ff"),
-            Color.valueOf("0x9814a0ff"), Color.valueOf("0xa31d9aff"),
-            Color.valueOf("0xad2693ff"), Color.valueOf("0xb6308bff"),
-            Color.valueOf("0xbf3984ff"), Color.valueOf("0xc7427cff"),
-            Color.valueOf("0xcf4c74ff"), Color.valueOf("0xd6556dff"),
-            Color.valueOf("0xdd5e66ff"), Color.valueOf("0xe3685fff"),
-            Color.valueOf("0xe97258ff"), Color.valueOf("0xee7c51ff"),
-            Color.valueOf("0xf3874aff"), Color.valueOf("0xf79243ff"),
-            Color.valueOf("0xfa9d3bff"), Color.valueOf("0xfca935ff"),
-            Color.valueOf("0xfdb52eff"), Color.valueOf("0xfdc229ff"),
-            Color.valueOf("0xfccf25ff"), Color.valueOf("0xf9dd24ff"),
-            Color.valueOf("0xf5eb27ff"), Color.valueOf("0xf0f921ff"));
-
 }
