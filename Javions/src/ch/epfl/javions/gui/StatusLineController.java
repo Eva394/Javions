a3b2f5242dc9f83cd1ba5e675df73a4b866305e8
nkim7 <nagyungecole@gmail.com>
2023-05-11 23:49:18 +0200

    StatusLineController part not done

---
+++ Javions/src/ch/epfl/javions/gui/StatusLineController.java
@@ -0,0 +1,55 @@
+package ch.epfl.javions.gui;
+
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableSet;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+public class StatusLineController {
+
+    private final BorderPane pane;
+    private final LongProperty aircraftCountProperty;
+    private final LongProperty messageCountProperty;
+
+    public StatusLineController(ObservableSet<ObservableAircraftState> aircraftStates,
+                                ObjectProperty<ObservableAircraftState> selectedAircraftState) {
+
+        AircraftTableController aircraftTableController = new AircraftTableController(aircraftStates, selectedAircraftState);
+        int numLines = aircraftTableController.getNumLines();
+
+        pane = new BorderPane();
+        pane.getStyleClass().add("status");
+
+        Text aircraftText = new Text();
+        aircraftText.textProperty()
+                .bind(new SimpleStringProperty("Visible aircraft: ")
+                        .concat(String.valueOf(numLines)));
+
+        Text messageText = new Text();
+        messageText.textProperty()
+                .bind(new SimpleStringProperty("Received messages: ")
+                        .concat());
+
+        pane.setLeft(aircraftText);
+        pane.setRight(messageText);
+
+        aircraftCountProperty = new SimpleLongProperty(0L);
+        messageCountProperty = new SimpleLongProperty(0L);
+    }
+
+    public BorderPane getPane() {
+        return pane;
+    }
+
+    public LongProperty aircraftCountProperty() {
+        return aircraftCountProperty;
+    }
+
+    public LongProperty messageCountProperty() {
+        return messageCountProperty;
+    }
+
+}}
