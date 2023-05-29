2c7ed266ed0ab549efc1d845dadbf755323a3647
Eva Mangano <eva.mangano@epfl.ch>
2023-05-29 15:19:14 +0200

    reverted commits v2

--- Javions/src/ch/epfl/javions/ByteString.java
+++ Javions/src/ch/epfl/javions/ByteString.java
@@ -7,115 +7,124 @@ import java.util.Objects;
 
 /**
  * A byte string (string of octets)
+ *
  * @author Eva Mangano 345375
  * @author Nagyung Kim (339628)
  */
 public final class ByteString {
-
-    private static final HexFormat HEX_FORMAT = HexFormat.of()
-                                                         .withUpperCase();
-    private final byte[] bytes;
-
-
-    /**
-     * gives a byte string whose content is that of the array passed as argument.
-     * @param bytes: array of bytes
-     */
-    public ByteString(byte[] bytes) {
-        this.bytes = bytes.clone();
-    }
-
-
-    /**
-     * Converts the given hexadecimal representation to a byte string of octets hexadecimal representation
-     * @param hexString hexadecimal representation of the value
-     * @return a byte string which is equal to the given hexadecimal representation
-     */
-    public static ByteString ofHexadecimalString(String hexString) {
-
-        byte[] tempBytes = HEX_FORMAT.parseHex( hexString );
-        return new ByteString( tempBytes );
-    }
-
-
-    /**
-     * returns the size of the string (the number of bytes it contains)
-     * @return the size of the string (the number of bytes it contains)
-     */
-    public int size() {
-
-        return bytes.length;
-    }
-
-
-    /**
-     * shows the bytes at the given index
-     * @param index index
-     * @return the byte at the given index
-     * @throws IndexOutOfBoundsException if the index is invalid
-     */
-    public int byteAt(int index) {
-
-        return Byte.toUnsignedInt( bytes[index] );
-    }
-
-
-    /**
-     * shows the bytes between the fromIndex and toIndex
-     * @param fromIndex the index indicates the start of the range (inclusive)
-     * @param toIndex   the index indicates the end of the range (exclusive)
-     * @return the bytes between the fromIndex and toIndex
-     * @throws IndexOutOfBoundsException if the ranged is not entirely between 0 and size of the string
-     * @throws IllegalArgumentException  if the difference between toIndex and fromIndex is not strictly less than the
-     *                                   number of bytes contained in a long type value
-     */
-    public long bytesInRange(int fromIndex, int toIndex) {
-        Objects.checkFromToIndex( fromIndex, toIndex, bytes.length );
-
-        long result = 0;
-        for ( int i = fromIndex ; i < toIndex ; i++ ) {
-            result = ( result << Byte.SIZE ) | byteAt( i );
-        }
-        return result;
-    }
-
-
-    /**
-     * returns the value returned by the hashCode method of Arrays
-     * @return the value returned by the hashCode method of Arrays applied to the array containing the bytes
-     */
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode( bytes );
-    }
-
-
-    /**
-     * Checks if the object is a ByteString and calls the Arrays equals method on the instance and the object, returns
-     * true in that case. Returns false if the object is not a ByteString or is not equal to the instance.
-     * @param obj object to compare to the instance
-     * @return a boolean, true if the object and the instance are equal
-     * @author Eva Mangano 345375
-     */
-    @Override
-    public boolean equals(Object obj) {
-
-        return obj instanceof ByteString byteString && Arrays.equals( byteString.bytes, this.bytes );
-    }
-
-
-    /**
-     * shows a representation of the bytes of the string in hexadecimal
-     * @return a representation of the bytes of the string in hexadecimal
-     */
-    @Override
-    public String toString() {
-
-        return HEX_FORMAT.formatHex( bytes );
-    }
-
-
-    private static boolean isEven(String hexString) {
-        return hexString.length() % 2 == 0;
-    }
+	
+	private static final HexFormat HEX_FORMAT = HexFormat.of()
+	                                                     .withUpperCase();
+	private final byte[] bytes;
+	
+	
+	/**
+	 * gives a byte string whose content is that of the array passed as argument.
+	 *
+	 * @param bytes: array of bytes
+	 */
+	public ByteString( byte[] bytes ) {
+		this.bytes = bytes.clone();
+	}
+	
+	
+	/**
+	 * Converts the given hexadecimal representation to a byte string of octets hexadecimal representation
+	 *
+	 * @param hexString hexadecimal representation of the value
+	 * @return a byte string which is equal to the given hexadecimal representation
+	 */
+	public static ByteString ofHexadecimalString( String hexString ) {
+		
+		byte[] tempBytes = HEX_FORMAT.parseHex( hexString );
+		return new ByteString( tempBytes );
+	}
+	
+	
+	/**
+	 * returns the size of the string (the number of bytes it contains)
+	 *
+	 * @return the size of the string (the number of bytes it contains)
+	 */
+	public int size() {
+		
+		return bytes.length;
+	}
+	
+	
+	/**
+	 * shows the bytes at the given index
+	 *
+	 * @param index index
+	 * @return the byte at the given index
+	 * @throws IndexOutOfBoundsException if the index is invalid
+	 */
+	public int byteAt( int index ) {
+		
+		return Byte.toUnsignedInt( bytes[ index ] );
+	}
+	
+	
+	/**
+	 * shows the bytes between the fromIndex and toIndex
+	 *
+	 * @param fromIndex the index indicates the start of the range (inclusive)
+	 * @param toIndex   the index indicates the end of the range (exclusive)
+	 * @return the bytes between the fromIndex and toIndex
+	 * @throws IndexOutOfBoundsException if the ranged is not entirely between 0 and size of the string
+	 * @throws IllegalArgumentException  if the difference between toIndex and fromIndex is not strictly less than the
+	 *                                   number of bytes contained in a long type value
+	 */
+	public long bytesInRange( int fromIndex, int toIndex ) {
+		Objects.checkFromToIndex( fromIndex, toIndex, bytes.length );
+		
+		long result = 0;
+		for ( int i = fromIndex ; i < toIndex ; i++ ) {
+			result = ( result << Byte.SIZE ) | byteAt( i );
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * returns the value returned by the hashCode method of Arrays
+	 *
+	 * @return the value returned by the hashCode method of Arrays applied to the array containing the bytes
+	 */
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode( bytes );
+	}
+	
+	
+	/**
+	 * Checks if the object is a ByteString and calls the Arrays equals method on the instance and the object, returns
+	 * true in that case. Returns false if the object is not a ByteString or is not equal to the instance.
+	 *
+	 * @param obj object to compare to the instance
+	 * @return a boolean, true if the object and the instance are equal
+	 * @author Eva Mangano 345375
+	 */
+	@Override
+	public boolean equals( Object obj ) {
+		
+		return obj instanceof ByteString byteString && Arrays.equals( byteString.bytes, this.bytes );
+	}
+	
+	
+	/**
+	 * shows a representation of the bytes of the string in hexadecimal
+	 *
+	 * @return a representation of the bytes of the string in hexadecimal
+	 */
+	@Override
+	public String toString() {
+		
+		return HEX_FORMAT.formatHex( bytes );
+	}
+	
+	
+	private static boolean isEven( String hexString ) {
+		return hexString.length() % 2 == 0;
+	}
 }
--- Javions/src/ch/epfl/javions/gui/Main.java
+++ Javions/src/ch/epfl/javions/gui/Main.java
@@ -1,7 +1,6 @@
 package ch.epfl.javions.gui;
 
 
-import ch.epfl.javions.ByteString;
 import ch.epfl.javions.adsb.Message;
 import ch.epfl.javions.adsb.MessageParser;
 import ch.epfl.javions.adsb.RawMessage;
@@ -22,195 +21,166 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public final class Main extends Application {
-
-    private static final int MIN_WIDTH = 800;
-    private static final int MIN_HEIGHT = 600;
-    private static final double ONE_SECOND_IN_NANOSECONDS = 1e9;
-    private static final int INITIAL_ZOOM_LEVEL = 8;
-    private static final int INITIAL_X = 33_530;
-    private static final int INITIAL_Y = 23_070;
-    private static final String SCENE_TITLE = "Javions";
-    private static final int MIN_SCENE_WIDTH = MIN_WIDTH;
-    private static final int MIN_SCENE_HEIGHT = MIN_HEIGHT;
-    private static final String SERVER_NAME = "tile.openstreetmap.org";
-    private static final String AIRCRAFT_RESOURCES = "/aircraft.zip";
-    private static final String CACHE_DIRECTORY = "Javions/tile-cache";
-
-
-    // TODO : the values in the table don't update + test with other file
-
-    @Override
-    public void start( Stage primaryStage ) throws Exception {
-
-        // Creation of the map's view
-        Path tileCache = Path.of( CACHE_DIRECTORY );
-        TileManager tileManager = new TileManager( tileCache, SERVER_NAME );
-        MapParameters mapParameters = new MapParameters( INITIAL_ZOOM_LEVEL, INITIAL_X, INITIAL_Y );
-        BaseMapController map = new BaseMapController( tileManager, mapParameters );
-
-        // Creation of the aircraft's view
-        URL databaseUrl = getClass().getResource( AIRCRAFT_RESOURCES );
-        assert databaseUrl != null;
-        String pathString = Path.of( databaseUrl.toURI() )
-                .toString();
-        AircraftDatabase aircraftDatabase = new AircraftDatabase( pathString );
-        AircraftStateManager aircraftStateManager = new AircraftStateManager( aircraftDatabase );
-        ObjectProperty<ObservableAircraftState> selectedAircraftStateProperty = new SimpleObjectProperty<>();
-        AircraftController aircraftController = new AircraftController( mapParameters, aircraftStateManager.states(),
-                selectedAircraftStateProperty );
-
-        // Creation of the table view
-        AircraftTableController table = new AircraftTableController( aircraftStateManager.states(),
-                selectedAircraftStateProperty );
-        table.setOnDoubleClick( observableAircraftState -> {
-            map.centerOn( observableAircraftState.getPosition() );
-            System.out.println( "in lambda - centering on" );
-        } );
-
-        // Creation of the status line view
-        StatusLineController statusLine = new StatusLineController();
-        statusLine.aircraftCountProperty()
-                .bind( Bindings.size( aircraftStateManager.states() ) );
-
-        // Creation of the leaf panes
-        StackPane mapAndAircraftPane = new StackPane( map.pane(), aircraftController.pane() );
-        BorderPane tableAndStatusLinePane = new BorderPane();
-        tableAndStatusLinePane.setCenter( table.pane() );
-        tableAndStatusLinePane.setTop( statusLine.pane() );
-
-        // Creation of the principal pane
-        SplitPane pane = new SplitPane( mapAndAircraftPane, tableAndStatusLinePane );
-        pane.setOrientation( Orientation.VERTICAL );
-//        pane.setMinWidth( MIN_WIDTH );
-//        pane.setMinHeight( MIN_HEIGHT );
-
-        // Creation of the scenes
-        Scene scene = new Scene( pane );
-        primaryStage.setScene( scene );
-        primaryStage.setTitle( SCENE_TITLE );
-        primaryStage.setMinWidth( MIN_SCENE_WIDTH );
-        primaryStage.setMinHeight( MIN_SCENE_HEIGHT );
-        primaryStage.show();
-
-        ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
-
-        Thread obtainingMessagesThread = new Thread( () -> {
-            try {
-                fillQueue( queue );
-            }
-            catch ( IOException e ) {
-                throw new UncheckedIOException( e );
-            }
-        } );
-        obtainingMessagesThread.setDaemon( true );
-        obtainingMessagesThread.start();
-
-        ( new AnimationTimer() {
-            private long lastPurgeTimeStampNs = 0L;
-
-            @Override
-            public void handle( long now ) {
-                try {
-                    while ( !queue.isEmpty() ) {
-                        Message message = queue.remove();
-                        statusLine.messageCountProperty().set( statusLine.messageCountProperty().get() + 1 );
-                        aircraftStateManager.updateWithMessage( message );
-                    }
-                    if ( now - lastPurgeTimeStampNs >= ONE_SECOND_IN_NANOSECONDS ) {
-                        aircraftStateManager.purge();
-                        lastPurgeTimeStampNs = now;
-                    }
-                    return;
-                }
-                catch ( IOException e ) {
-                    throw new UncheckedIOException( e );
-                }
-            }
-        } ).start();
-    }
-
-    private void fillQueue( ConcurrentLinkedQueue<Message> queue ) throws IOException {
-        long lastMessageTimeStampNs = 0L;
-        String fileName;
-        Message message;
-        fileName = getParameters().getRaw()
-                .get( 0 );
-
-        // the messages come from a file
-        if ( fileName != null ) {
-//            List<RawMessage> rawMessages = readAllMessages( fileName );
-
-            byte[] bytes = new byte[ RawMessage.LENGTH ];
-
-            try ( DataInputStream s = new DataInputStream( new BufferedInputStream( new FileInputStream( fileName ) ) ) ) {
-                while ( true ) {
-                    long timeStampNs = s.readLong();
-                    int bytesRead = s.readNBytes( bytes, 0, bytes.length );
-                    assert bytesRead == RawMessage.LENGTH;
-
-                    ByteString messageByteString = new ByteString( bytes );
-                    assert messageByteString != null;
-
-                    RawMessage rawMessage;
-                    if ( ( rawMessage = RawMessage.of( timeStampNs, bytes ) ) != null && ( message = MessageParser.parse( rawMessage ) ) != null ) {
-                        Thread.sleep( ( long ) ( ( message.timeStampNs() - lastMessageTimeStampNs ) * 1e-6 ) );
-                        queue.add( message );
-                        lastMessageTimeStampNs = message.timeStampNs();
-                    }
-                    else {
-                        System.out.println( "queue.size() = " + queue.size() );
-                        break;
-                    }
-                }
-            }
-            catch ( IOException ignored ) {
-            }
-            catch ( InterruptedException e ) {
-                throw new Error( e );
-            }
-        }
-        // the messages come from the radio
-        else {
-            AdsbDemodulator adsbDemodulator = new AdsbDemodulator( System.in );
-            RawMessage rawMessage;
-            while ( ( rawMessage = adsbDemodulator.nextMessage() ) != null ) {
-                if ( ( message = MessageParser.parse( rawMessage ) ) != null ) {
-                    queue.add( message );
-                }
-            }
-        }
-    }
-
-    public static void main( String[] args ) {
-        Application.launch( args );
-    }
-
-//    private static List<RawMessage> readAllMessages( String fileName ) {
-//        List<RawMessage> list = new ArrayList<>();
-//
-//        byte[] bytes = new byte[ RawMessage.LENGTH ];
-//
-//        try ( DataInputStream s = new DataInputStream( new BufferedInputStream( new FileInputStream( fileName ) ) ) ) {
-//            while ( true ) {
-//                long timeStampNs = s.readLong();
-//                int bytesRead = s.readNBytes( bytes, 0, bytes.length );
-//                assert bytesRead == RawMessage.LENGTH;
-//
-//                ByteString messageByteString = new ByteString( bytes );
-//                assert messageByteString != null;
-//
-//                RawMessage rawMessage;
-//                if ( ( rawMessage = RawMessage.of( timeStampNs, bytes ) ) != null && ) {
-//                    list.add( rawMessage );
-//                }
-//            }
-//        }
-//        catch ( IOException ignored ) {
-//            System.out.println( "list.size() = " + list.size() );
-//            return list;
-//        }
-//    }
+	
+	private static final int MIN_WIDTH = 800;
+	private static final int MIN_HEIGHT = 600;
+	private static final double ONE_SECOND_IN_NANOSECONDS = 1e9;
+	private static final int INITIAL_ZOOM_LEVEL = 8;
+	private static final int INITIAL_X = 33_530;
+	private static final int INITIAL_Y = 23_070;
+	private static final String SCENE_TITLE = "Javions";
+	private static final int MIN_SCENE_WIDTH = MIN_WIDTH;
+	private static final int MIN_SCENE_HEIGHT = MIN_HEIGHT;
+	private static final String SERVER_NAME = "tile.openstreetmap.org";
+	private static final String AIRCRAFT_RESOURCES = "/aircraft.zip";
+	private static final String CACHE_DIRECTORY = "Javions/tile-cache";
+	private static final double NS_TO_MS_FACTOR = 1e-6;
+	
+	@Override
+	public void start( Stage primaryStage ) throws Exception {
+		
+		// Creation of the map's view
+		Path tileCache = Path.of( CACHE_DIRECTORY );
+		TileManager tileManager = new TileManager( tileCache, SERVER_NAME );
+		MapParameters mapParameters = new MapParameters( INITIAL_ZOOM_LEVEL, INITIAL_X, INITIAL_Y );
+		BaseMapController map = new BaseMapController( tileManager, mapParameters );
+		
+		// Creation of the aircraft's view
+		URL databaseUrl = getClass().getResource( AIRCRAFT_RESOURCES );
+		assert databaseUrl != null;
+		String pathString = Path.of( databaseUrl.toURI() )
+		                        .toString();
+		AircraftDatabase aircraftDatabase = new AircraftDatabase( pathString );
+		AircraftStateManager aircraftStateManager = new AircraftStateManager( aircraftDatabase );
+		ObjectProperty<ObservableAircraftState> selectedAircraftStateProperty = new SimpleObjectProperty<>();
+		AircraftController aircraftController = new AircraftController( mapParameters, aircraftStateManager.states(),
+		                                                                selectedAircraftStateProperty );
+		
+		// Creation of the table view
+		AircraftTableController table = new AircraftTableController( aircraftStateManager.states(),
+		                                                             selectedAircraftStateProperty );
+		table.setOnDoubleClick( observableAircraftState -> {
+			map.centerOn( observableAircraftState.getPosition() );
+		} );
+		
+		// Creation of the status line view
+		StatusLineController statusLine = new StatusLineController();
+		statusLine.aircraftCountProperty()
+		          .bind( Bindings.size( aircraftStateManager.states() ) );
+		
+		// Creation of the leaf panes
+		StackPane mapAndAircraftPane = new StackPane( map.pane(), aircraftController.pane() );
+		BorderPane tableAndStatusLinePane = new BorderPane();
+		tableAndStatusLinePane.setCenter( table.pane() );
+		tableAndStatusLinePane.setTop( statusLine.pane() );
+		
+		// Creation of the principal pane
+		SplitPane pane = new SplitPane( mapAndAircraftPane, tableAndStatusLinePane );
+		pane.setOrientation( Orientation.VERTICAL );
+		
+		// Creation of the scenes
+		Scene scene = new Scene( pane );
+		primaryStage.setScene( scene );
+		primaryStage.setTitle( SCENE_TITLE );
+		primaryStage.setMinWidth( MIN_SCENE_WIDTH );
+		primaryStage.setMinHeight( MIN_SCENE_HEIGHT );
+		primaryStage.show();
+		
+		ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
+		
+		Thread obtainingMessagesThread = new Thread( () -> {
+			try {
+				fillQueue( queue );
+			}
+			catch ( IOException e ) {
+				throw new UncheckedIOException( e );
+			}
+		} );
+		obtainingMessagesThread.setDaemon( true );
+		obtainingMessagesThread.start();
+		
+		( new AnimationTimer() {
+			private long lastPurgeTimeStampNs = 0L;
+			
+			@Override
+			public void handle( long now ) {
+				try {
+					while ( !queue.isEmpty() ) {
+						Message message = queue.remove();
+						statusLine.messageCountProperty()
+						          .set( statusLine.messageCountProperty()
+						                          .get() + 1L );
+						aircraftStateManager.updateWithMessage( message );
+					}
+					if ( now - lastPurgeTimeStampNs >= ONE_SECOND_IN_NANOSECONDS ) {
+						aircraftStateManager.purge();
+						lastPurgeTimeStampNs = now;
+					}
+					return;
+				}
+				catch ( IOException e ) {
+					throw new UncheckedIOException( e );
+				}
+			}
+		} ).start();
+	}
+	
+	public static void main( String[] args ) {
+		Application.launch( args );
+	}
+	
+	private void fillQueue( ConcurrentLinkedQueue<Message> queue ) throws IOException {
+		long lastMessageTimeStampNs = 0L;
+		String fileName;
+		List<String> parametersString;
+		Message message;
+		
+		// the messages come from a file
+		if ( !( parametersString = getParameters().getRaw() ).isEmpty() && ( fileName = parametersString.get( 0 ) ) != null ) {
+			
+			byte[] bytes = new byte[ RawMessage.LENGTH ];
+			
+			try ( DataInputStream s = new DataInputStream( new BufferedInputStream( new FileInputStream( fileName ) ) ) ) {
+				long timeStampNs;
+				int bytesRead;
+				RawMessage rawMessage;
+				
+				while ( true ) {
+					timeStampNs = s.readLong();
+					bytesRead = s.readNBytes( bytes, 0, bytes.length );
+					assert bytesRead == RawMessage.LENGTH;
+					
+					if ( ( rawMessage = RawMessage.of( timeStampNs, bytes ) ) != null ) {
+						if ( ( message = MessageParser.parse( rawMessage ) ) != null ) {
+							Thread.sleep( ( long ) ( ( message.timeStampNs() - lastMessageTimeStampNs ) * NS_TO_MS_FACTOR ) );
+							queue.add( message );
+							lastMessageTimeStampNs = message.timeStampNs();
+						}
+					}
+					else {
+						break;
+					}
+				}
+			}
+			catch ( IOException ignored ) {
+			}
+			catch ( InterruptedException e ) {
+				throw new Error( e );
+			}
+		}
+		// the messages come from the radio
+		else {
+			AdsbDemodulator adsbDemodulator = new AdsbDemodulator( System.in );
+			RawMessage rawMessage;
+			while ( ( rawMessage = adsbDemodulator.nextMessage() ) != null ) {
+				if ( ( message = MessageParser.parse( rawMessage ) ) != null ) {
+					queue.add( message );
+				}
+			}
+		}
+	}
 }
