a81adf63d98c72cf1174b2fb8aefe532f95de36d
Eva Mangano <eva.mangano@epfl.ch>
2023-05-31 13:49:42 +0200

    tokens

--- Javions/src/ch/epfl/sigcheck/SignatureChecks_11.java
+++ Javions/src/ch/epfl/sigcheck/SignatureChecks_11.java
@@ -5,23 +5,17 @@ package ch.epfl.sigcheck;
 // possible, que les noms et les types des différentes entités à définir
 // pour cette étape du projet sont corrects.
 
-
-
-
 final class SignatureChecks_11 {
-
-    ch.epfl.javions.gui.Main v01;
-    java.lang.String[] v02;
-    javafx.stage.Stage v03;
-
-
-    private SignatureChecks_11() {
-    }
-
-
-    void checkMain() throws Exception {
-        v01 = new ch.epfl.javions.gui.Main();
-        ch.epfl.javions.gui.Main.main( v02 );
-        v01.start( v03 );
-    }
+	ch.epfl.javions.gui.Main v01;
+	String[] v02;
+	javafx.stage.Stage v03;
+	
+	private SignatureChecks_11() {
+	}
+	
+	void checkMain() throws Exception {
+		v01 = new ch.epfl.javions.gui.Main();
+		ch.epfl.javions.gui.Main.main( v02 );
+		v01.start( v03 );
+	}
 }
